c39aac2582db976fb7d040ef7d9d9f9337318cc4
Piotr Rzeszut <piotrrze@gmail.com>
2023-11-01 22:07:45 +0100

    Wotking menu using NEC-compatible remote & multiplexing display working.

--- nixie_firmware/Core/Src/main.c
+++ nixie_firmware/Core/Src/main.c
@@ -26,6 +26,15 @@
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
+typedef enum {
+  REMOTE_IDLE,
+  REMOTE_INC_H,
+  REMOTE_DEC_H,
+  REMOTE_INC_M,
+  REMOTE_DEC_M,
+  REMOTE_MENU,
+  REMOTE_EXIT
+} remoteAction_t;
 
 /* USER CODE END PTD */
 
@@ -47,7 +56,9 @@ TIM_HandleTypeDef htim2;
 
 /* USER CODE BEGIN PV */
 RTC_TimeTypeDef time;
-
+volatile uint8_t signs[4] = {1, 2, 3, 4};
+volatile bool signDot = true;
+volatile remoteAction_t remoteAction = REMOTE_IDLE;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -72,7 +83,10 @@ static void MX_TIM2_Init(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
+  uint32_t lastTickDot = 0;
+  uint32_t lastTickRefresh = 0;
+  uint32_t lastTickMenu = 0;
+  bool inMenu = false;
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -98,13 +112,107 @@ int main(void)
   MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
   HAL_TIM_Base_Start(&htim1);
+  HAL_TIM_Base_Start_IT(&htim2);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    HAL_RTC_GetTime(&hrtc, &time, RTC_FORMAT_BCD);
+    /* refresh every 25ms */
+    if (HAL_GetTick() - lastTickRefresh > 25)
+    {
+      lastTickRefresh = HAL_GetTick();
+
+      /* if more than 15s without remote activity - turn of menu */
+      if (HAL_GetTick() - lastTickMenu > 15000)
+      {
+        inMenu = false;
+      }
+
+      /* toggle dot every 500ms (100ms when in menu) */
+      if (HAL_GetTick() - lastTickDot > (inMenu?100:500))
+      {
+        lastTickDot = HAL_GetTick();
+        __disable_irq();
+        signDot = !signDot;
+        __enable_irq();
+      }
+
+      HAL_RTC_GetTime(&hrtc, &time, RTC_FORMAT_BIN);
+
+      __disable_irq();
+      signs[0] = time.Hours/10;
+      signs[1] = time.Hours%10;
+      signs[2] = time.Minutes/10;
+      signs[3] = time.Minutes%10;
+
+      if (remoteAction != REMOTE_IDLE)
+      {
+        lastTickMenu = HAL_GetTick();
+        switch(remoteAction)
+        {
+          case REMOTE_DEC_H:
+            if (time.Hours > 0)
+            {
+              time.Hours--;
+            }
+            else
+            {
+              time.Hours = 23;
+            }
+            time.Seconds = 0;
+            break;
+          case REMOTE_INC_H:
+            if (time.Hours < 23)
+            {
+              time.Hours++;
+            }
+            else
+            {
+              time.Hours = 0;
+            }
+            time.Seconds = 0;
+            break;
+          case REMOTE_DEC_M:
+            if (time.Minutes > 0)
+            {
+              time.Minutes--;
+            }
+            else
+            {
+              time.Minutes = 59;
+            }
+            time.Seconds = 0;
+            break;
+          case REMOTE_INC_M:
+            if (time.Minutes < 59)
+            {
+              time.Minutes++;
+            }
+            else
+            {
+              time.Minutes = 0;
+            }
+            time.Seconds = 0;
+            break;
+          case REMOTE_EXIT:
+            inMenu = false;
+            break;
+          default:
+          case REMOTE_IDLE:
+            break;
+        }
+        if (inMenu) HAL_RTC_SetTime(&hrtc, &time, RTC_FORMAT_BIN);
+        if (!inMenu && remoteAction == REMOTE_MENU)
+        {
+          inMenu = true;
+        }
+        remoteAction = REMOTE_IDLE;
+      }
+
+      __enable_irq();
+    }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -125,10 +233,10 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_LSE;
-  RCC_OscInitStruct.LSEState = RCC_LSE_ON;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_LSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
   RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
@@ -151,7 +259,7 @@ void SystemClock_Config(void)
     Error_Handler();
   }
   PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_RTC;
-  PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
+  PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSI;
   if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
   {
     Error_Handler();
@@ -292,12 +400,12 @@ static void MX_GPIO_Init(void)
 /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, DIGIT_1_Pin|DIGIT_2_Pin|DIGIT_3_Pin|DIGIT_4_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, DIGIT_1_Pin|DIGIT_2_Pin|DIGIT_3_Pin|DIGIT_4_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOC, SIGN_0_Pin|SIGN_9_Pin|SIGN_8_Pin|SIGN_7_Pin, GPIO_PIN_RESET);
@@ -383,10 +491,39 @@ necPulse_t necPulseDecode(uint16_t pulse_us)
 }
 
 #define NEC_BITS 32
+#define NEC_DEBOUNCE_MS 250
 
 void necCallback(uint8_t address, uint8_t command)
 {
-  address = command;
+  static uint32_t debounce = 0;
+  if (address == 0x77 && (HAL_GetTick() - debounce > NEC_DEBOUNCE_MS))
+  {
+    debounce = HAL_GetTick();
+    switch (command)
+    {
+      case 0x8D: /* DOWN */
+        remoteAction = REMOTE_INC_H;
+        break;
+      case 0x8E: /* UP */
+        remoteAction = REMOTE_DEC_H;
+        break;
+      case 0x90: /* VOL+ */
+        remoteAction = REMOTE_INC_M;
+        break;
+      case 0x8F: /* VOL- */
+        remoteAction = REMOTE_DEC_M;
+        break;
+      case 0x95: /* MENU */
+        remoteAction = REMOTE_MENU;
+        break;
+      case 0x96: /* EXIT */
+        remoteAction = REMOTE_EXIT;
+        break;
+      default:
+        remoteAction = REMOTE_IDLE;
+        break;
+    }
+  }
 }
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
@@ -434,6 +571,147 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
         {
           necCallback(address, command);
         }
+      }
+    }
+  }
+}
+
+typedef enum {
+  NO_DOT,
+  DOT_0,
+  DOT_1
+} dot_t;
+
+#define SIGN_DISABLE 10
+#define SIGN_BLANK   99
+
+void setSign(uint8_t sign, dot_t dot)
+{
+  /* for DOT we use SIGN_0 line of the 10's hour digit and the digit does not display 0 */
+  if (dot != NO_DOT && sign == 0)
+  {
+    sign = SIGN_DISABLE;
+  }
+  switch(sign)
+  {
+    case 0:
+      HAL_GPIO_WritePin(SIGN_0_GPIO_Port, SIGN_0_Pin, GPIO_PIN_SET);
+      break;
+    case 1:
+      //HAL_GPIO_WritePin(SIGN_1_GPIO_Port, SIGN_1_Pin, GPIO_PIN_SET); /* TODO: SIGN_1 line colides with SWD interface */
+      break;
+    case 2:
+      HAL_GPIO_WritePin(SIGN_2_GPIO_Port, SIGN_2_Pin, GPIO_PIN_SET);
+      break;
+    case 3:
+      HAL_GPIO_WritePin(SIGN_3_GPIO_Port, SIGN_3_Pin, GPIO_PIN_SET);
+      break;
+    case 4:
+      HAL_GPIO_WritePin(SIGN_4_GPIO_Port, SIGN_4_Pin, GPIO_PIN_SET);
+      break;
+    case 5:
+      HAL_GPIO_WritePin(SIGN_5_GPIO_Port, SIGN_5_Pin, GPIO_PIN_SET);
+      break;
+    case 6:
+      HAL_GPIO_WritePin(SIGN_6_GPIO_Port, SIGN_6_Pin, GPIO_PIN_SET);
+      break;
+    case 7:
+      HAL_GPIO_WritePin(SIGN_7_GPIO_Port, SIGN_7_Pin, GPIO_PIN_SET);
+      break;
+    case 8:
+      HAL_GPIO_WritePin(SIGN_8_GPIO_Port, SIGN_8_Pin, GPIO_PIN_SET);
+      break;
+    case 9:
+      HAL_GPIO_WritePin(SIGN_9_GPIO_Port, SIGN_9_Pin, GPIO_PIN_SET);
+      break;
+    case SIGN_DISABLE:
+      break;
+    case SIGN_BLANK:
+    default:
+      HAL_GPIO_WritePin(SIGN_0_GPIO_Port, SIGN_0_Pin, GPIO_PIN_RESET);
+      //HAL_GPIO_WritePin(SIGN_1_GPIO_Port, SIGN_1_Pin, GPIO_PIN_RESET); /* TODO: SIGN_1 line colides with SWD interface */
+      HAL_GPIO_WritePin(SIGN_2_GPIO_Port, SIGN_2_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(SIGN_3_GPIO_Port, SIGN_3_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(SIGN_4_GPIO_Port, SIGN_4_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(SIGN_5_GPIO_Port, SIGN_5_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(SIGN_6_GPIO_Port, SIGN_6_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(SIGN_7_GPIO_Port, SIGN_7_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(SIGN_8_GPIO_Port, SIGN_8_Pin, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(SIGN_9_GPIO_Port, SIGN_9_Pin, GPIO_PIN_RESET);
+      break;
+  }
+  /* if DOT is to be activated */
+  if (dot == DOT_1)
+  {
+    HAL_GPIO_WritePin(SIGN_0_GPIO_Port, SIGN_0_Pin, GPIO_PIN_SET);
+  }
+}
+
+#define DIGIT_BLANK 99
+
+void setDigit(uint8_t digit)
+{
+  switch(digit)
+  {
+    case 0:
+      HAL_GPIO_WritePin(DIGIT_1_GPIO_Port, DIGIT_1_Pin, GPIO_PIN_RESET);
+      break;
+    case 1:
+      HAL_GPIO_WritePin(DIGIT_2_GPIO_Port, DIGIT_2_Pin, GPIO_PIN_RESET);
+      break;
+    case 2:
+      HAL_GPIO_WritePin(DIGIT_3_GPIO_Port, DIGIT_3_Pin, GPIO_PIN_RESET);
+      break;
+    case 3:
+      HAL_GPIO_WritePin(DIGIT_4_GPIO_Port, DIGIT_4_Pin, GPIO_PIN_RESET);
+      break;
+    case DIGIT_BLANK:
+    default:
+      HAL_GPIO_WritePin(DIGIT_1_GPIO_Port, DIGIT_1_Pin, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(DIGIT_2_GPIO_Port, DIGIT_2_Pin, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(DIGIT_3_GPIO_Port, DIGIT_3_Pin, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(DIGIT_4_GPIO_Port, DIGIT_4_Pin, GPIO_PIN_SET);
+      break;
+  }
+}
+
+#define BLANK_MS  1
+#define ACTIVE_MS 2
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  static uint8_t digit = 0;
+  static bool isBlanking = true;
+  static uint16_t counter = 0;
+  if (htim == &htim2)
+  {
+    if (isBlanking)
+    {
+      if (counter == 0)
+      {
+        setDigit(digit);
+        setSign(signs[digit], (digit==0)?(signDot?DOT_1:DOT_0):NO_DOT);
+        digit = (digit + 1) % 4;
+        isBlanking = false;
+        counter = ACTIVE_MS - 1;
+      }
+      else
+      {
+        counter--;
+      }
+    }
+    else /* not blanking */
+    {
+      if (counter == 0)
+      {
+        setSign(SIGN_BLANK, NO_DOT);
+        setDigit(DIGIT_BLANK);
+        isBlanking = true;
+        counter = BLANK_MS - 1;
+      }
+      else
+      {
+        counter--;
       }
     }
   }
