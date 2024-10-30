97da9a38c10c086bb4d4d78f00e1129be35fd4d2
Piotr Rzeszut <piotrrze@gmail.com>
2024-10-30 23:38:04 +0100

    Working NTP demo

---
+++ NTP_sync_ESP32/NTP_sync/include/credentials_template.h
@@ -0,0 +1,10 @@
+/*
+ * Please rename this file to credentials.h and put your SSID and PASSPHRASE in corresponding places
+ */
+#ifndef __CREDENTIALS_H__
+#define __CREDENTIALS_H__
+
+#define WIFI_SSDI "YOUR_WIFI_SSID"
+#define WIFI_PASS "YOUR_WIFI_PASSPHRASE"
+
+#endif  // __CREDENTIALS_H__
--- NTP_sync_ESP32/NTP_sync/src/main.cpp
+++ NTP_sync_ESP32/NTP_sync/src/main.cpp
@@ -1,7 +1,55 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <time.h>
+
+#include "credentials.h"
+
+void printLocalTime(){
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+}
+
+void setTimezone(String timezone){
+  Serial.printf("  Setting Timezone to %s\n",timezone.c_str());
+  Serial.println("");
+  setenv("TZ",timezone.c_str(),1);  //  Now adjust the TZ.  Clock settings are adjusted to show the new local time
+  tzset();
+}
+
+void initTime(String timezone){
+  struct tm timeinfo;
+
+  Serial.println("Setting up time");
+  configTime(0, 0, "pool.ntp.org");    // First connect to NTP server, with 0 TZ offset
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("  Failed to obtain time");
+    return;
+  }
+  Serial.println("  Got the time from NTP");
+  // Now we can set the real timezone
+  setTimezone(timezone);
+}
 
 void setup() {
+  Serial.begin(115200);
+  Serial1.begin(115200, SERIAL_8N1, 20, 21);
   pinMode(LED_BUILTIN, OUTPUT);
+  WiFi.begin(WIFI_SSDI, WIFI_PASS);
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSDI);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(250);
+    Serial.print(".");
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  }
+  Serial.println("");
+  Serial.println("WiFi connected.");
+
+  initTime("CET-1CEST,M3.5.0,M10.5.0/3");   // Set for Europe/Warsaw
 }
 
 void loop() {
@@ -9,4 +57,6 @@ void loop() {
   digitalWrite(LED_BUILTIN, HIGH);
   delay(500);
   digitalWrite(LED_BUILTIN, LOW);
+  Serial1.println("QUE?");
+  printLocalTime();
 }
